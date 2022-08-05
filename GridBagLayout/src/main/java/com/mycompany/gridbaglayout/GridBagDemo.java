fe19ab40051e6727628d5ba772b0d17194e826cf
Victor Henrique <41922049+Squivoix@users.noreply.github.com>
2022-08-05 20:06:40 -0300

    Add files via upload

---
+++ GridBagLayout/src/main/java/com/mycompany/gridbaglayout/GridBagDemo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gridbaglayout;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Victor Henrique
+ */
+public class GridBagDemo {
+    public static void main(String[] args) {
+        GridBagFrame2 gridBagFrame = new GridBagFrame2();
+        gridBagFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        gridBagFrame.setSize(300, 200);
+        gridBagFrame.setVisible(true);
+    }
+}
